0a7aa32a3a44aa800740699165d3fd0fea474514
Amy H <amhill@bellsouth.net>
2017-04-19 14:11:48 -0500

    created loadMap method.

--- app/src/main/java/edu/apsu/csci/teamaz/dungeoncrawler/Map.java
+++ app/src/main/java/edu/apsu/csci/teamaz/dungeoncrawler/Map.java
@@ -5,6 +5,14 @@ import android.graphics.Canvas;
 import android.graphics.Point;
 import android.util.Log;
 import android.util.Size;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.Scanner;
 
 import edu.apsu.csci.teamaz.dungeoncrawler.worldobjects.GenericEntity;
 import edu.apsu.csci.teamaz.dungeoncrawler.worldobjects.WorldObject;
@@ -50,30 +58,98 @@ public class Map {
     }
 
 
-    //creates a map based on the given dimensions
-    //will put wall and wall_corners on the outer most edge
-    private void initalizeMap(int height, int width) {
-        map = new GenericEntity[height][width];
-        for (int row = 0; row < map.length; row++) {
-            for (int col = 0; col < map[row].length; col++) {
-                if (map[row][col] != null) {
-                    Log.i("=============", "map ");
-                    if ((row == 0 && col == 0) || (row == 0 && col == map[row].length - 1) ||
-                            (col == 0 && row == map.length - 1) ||
-                            (col == map[row].length - 1 && map.length - 1 == row)) {
+    //creates a map based on a given file, which has
+    //numbers that corresponds to a specific image and rotation
+    private void loadMap(String filename){
+        int height, width;
+        Point point = new Point(0,0);
 
-                        map[row][col].setDrawable(R.drawable.wall_corner);
-                    } else if (row == 0 || row == map.length - 1 || col == 0 || col == map[row].length - 1) {
-                        map[row][col].setDrawable(R.drawable.wall);
-                    } else {
-                        map[row][col].setDrawable(R.drawable.floor);
-                    }
-                } else {
-                    Log.i("=============", "map not intialized");
-                }
-
-            }
+        Scanner scanner;
+        try {
+            scanner = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            Log.i("============", "Unable to load " + filename);
+            Toast.makeText(context, "Unable to load map", Toast.LENGTH_SHORT).show();
+            scanner = null;
         }
+        if(scanner != null) {
+            width = scanner.nextInt();
+            height = scanner.nextInt();
+
+            map = new WorldObject[width][height];
+
+            for (int row = 0; row < map.length; row++) {
+                for (int col = 0; col < map[row].length; col++) {
+                    map[row][col] = loadMapHelper(scanner.nextInt(), point);
+                    point.x += 300;
+                }
+                point.y += 300;
+            }
+            scanner.close();
+        }
+    }
+    private WorldObject loadMapHelper(int objectNumber, Point point){
+        int drawableId = 0;
+        int rotation = 0;
+        boolean passable = false;
+
+        /*
+            1 = floor
+            2-5 = wall
+            6-9 = door
+            10-13 = wall_corner
+         */
+        switch (objectNumber){
+            case 1:
+                drawableId = R.drawable.floor;
+                passable = true;
+                break;
+            case 2:
+                drawableId = R.drawable.wall;
+                rotation = 180;
+                break;
+            case 3:
+                drawableId = R.drawable.wall;
+                rotation = 270;
+                break;
+            case 4:
+                drawableId = R.drawable.wall;
+                rotation = 0;
+                break;
+            case 5:
+                drawableId = R.drawable.wall;
+                rotation = 90;
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                drawableId = R.drawable.wall_corner;
+                rotation = 90;
+                break;
+            case 11:
+                drawableId = R.drawable.wall_corner;
+                rotation = 0;
+                break;
+            case 12:
+                drawableId = R.drawable.wall_corner;
+                rotation = 270;
+                break;
+            case 13:
+                drawableId = R.drawable.wall_corner;
+                rotation = 180;
+                break;
+        }
+        WorldObject worldObject = new WorldObject(point,rotation,TILE_SIZE, context, passable);
+        if(drawableId != 0) {
+            worldObject.setDrawable(drawableId);
+        }
+        return  worldObject;
     }
 
     //Creates a one room map for testing purposes.
