b2b5be9240a22519299536a90ddf486a9e98b194
Zachary Taylor <taylor.zachary11@gmail.com>
2017-04-21 21:57:52 -0500

    Cleaned and commented code

--- app/src/main/java/edu/apsu/csci/teamaz/dungeoncrawler/Game.java
+++ app/src/main/java/edu/apsu/csci/teamaz/dungeoncrawler/Game.java
@@ -2,10 +2,7 @@ package edu.apsu.csci.teamaz.dungeoncrawler;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.drawable.GradientDrawable;
 import android.util.Log;
 import android.util.Size;
 
@@ -15,21 +12,29 @@ import edu.apsu.csci.teamaz.dungeoncrawler.worldobjects.GenericEntity;
 import edu.apsu.csci.teamaz.dungeoncrawler.worldobjects.PlayerEntity;
 
 public class Game {
+    private Map map;
+    private ArrayList<GenericEntity> obstacles;
+    private PlayerEntity player;
+
+    /* Constructor(s) */
+    /***********************/
     public Game(Size size, Context context, PlayerEntity player) {
-        map = new Map(new Size(4,4), context, "testmap.csv");
-        //Log.i("=============", "game size: " + size.getHeight() + " " + size.getWidth());
-        enemies = new ArrayList<>();
+        this.map = new Map(new Size(4,4), context, "testmap.csv");
+        this.obstacles = new ArrayList<>();
         this.player = player;
 
-//        map.makeTestMap();
-
+        /* Testing code for player location. */
         this.player.setMapLocation(map.getCenter());
+        //Log.i("=============", "game size: " + size.getHeight() + " " + size.getWidth());
     }
 
+    /* Methods */
+    /***********************/
+    /* UpdateWorldObjects moves all the movable entities around.*/
     public void updateWorldObjects(){
         Point testPoint;
         for (GenericEntity enemy:
-             enemies) {
+                obstacles) {
             testPoint = enemy.calculateNextLocation(0);
             if(map.checkCollision(testPoint)){
                 enemy.setMapLocation(testPoint);
@@ -53,50 +58,62 @@ public class Game {
         }
     }
 
+    /* Passes Draw calls to respective entities. */
     public void draw(Canvas canvas){
+        /* Sets the canvas 0,0 location to the players render location. */
         canvas.translate(player.getRenderLocation().x, player.getRenderLocation().y);
+
+        /* This section tells each part to draw from map to obstacles and finally the player. */
         map.draw(canvas, player.getMapLocation_dp());
 
         for (GenericEntity enemy:
-                enemies) {
+                obstacles) {
             enemy.draw(canvas, player.getMapLocation_dp());
         }
+
         // Log.i("=================", player.toString());
         if(player != null) {
-            //Log.i("=================", "In game ondraw");
             player.draw(canvas, null);
+            //Log.i("=================", "In game ondraw");
         }
 
     }
 
-    //methods
-
-    //Distance Formula
-    public double getNumSteps(Point p1, Point p2, int stepSize){
-        int x = p2.x - p1.x;
-        int y = p2.y - p1.y;
-
-
-        double distance = Math.abs(Math.sqrt(x*x + y*y)) / player.getScale();
-        Log.i("Distance between p1/p2", "" + distance);
-        return  distance / stepSize;
-    }
-
+    /* Moves the player from their current location to 'targetPoint'. */
     public void movePlayerTo(Point targetPoint){
-        player.updateRotation(targetPoint);
-        player.setMoving(true);
-
-        Log.i("Player Debug", targetPoint.toString());
-        player.setNumberSteps(getNumSteps(player.getRenderLocation(), targetPoint, player.getStep()));
+        player.moveTo(targetPoint);
+        //Log.i("Player Debug", targetPoint.toString());
     }
 
-    //setters
+    /* Getters and Setters */
+    /***********************/
+
+    /* Obstacles for the map including doors. */
+    public ArrayList<GenericEntity> getObstacles() {
+        return obstacles;
+    }
+
+    public void setObstacles(ArrayList<GenericEntity> obstacles) {
+        this.obstacles = obstacles;
+    }
+
+    /* User Character for the game. */
     public void setPlayer(PlayerEntity player){
         this.player = player;
     }
 
-    //Fields
-    private Map map;
-    private ArrayList<GenericEntity> enemies;
-    private PlayerEntity player;
+    public PlayerEntity getPlayer() {
+        return player;
+    }
+
+    /* Code representation of the map. */
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+
 }
--- app/src/main/java/edu/apsu/csci/teamaz/dungeoncrawler/Map.java
+++ app/src/main/java/edu/apsu/csci/teamaz/dungeoncrawler/Map.java
@@ -7,88 +7,91 @@ import android.util.Log;
 import android.util.Size;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Scanner;
 
-import edu.apsu.csci.teamaz.dungeoncrawler.worldobjects.GenericEntity;
 import edu.apsu.csci.teamaz.dungeoncrawler.worldobjects.WorldObject;
 
 public class Map {
     public static final Size TILE_SIZE = new Size(900, 900);
-    public static final double SIZE = 900;
+    public static final int SIZE = 900;
+
     private WorldObject[][] map;
     private Context context;
 
-
+    /* Constructor(s) */
+    /***********************/
     public Map(Size size, Context context, String mapName) {
         this.context = context;
         loadMap(mapName);
     }
 
-    //Methods
+    /* Method(s) */
+    /***********************/
+    /* Draw tells each map object to draw on the given canvas. */
     public void draw(Canvas canvas, Point playerLocation) {
         if (map != null) {
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[i].length; j++) {
-                    if (map[i][j] != null) {
-                        map[i][j].draw(canvas, playerLocation);
+            for(WorldObject[] col  : map){
+                for(WorldObject item: col){
+                    if (item != null) {
+                        item.draw(canvas, playerLocation);
                     }
                 }
-
             }
         }
     }
 
+    /* Checks if the given point is within the bounds of the object.
+     *  This may need to be moved to worldobject.java
+     */
     public boolean checkCollision(Point targetPoint) {
         int x, y;
-        x = targetPoint.x / TILE_SIZE.getWidth();
-        y = targetPoint.y / TILE_SIZE.getHeight();
+        x = targetPoint.x / SIZE;
+        y = targetPoint.y / SIZE;
         //Log.i("=============", "Target Point:" + targetPoint.x + " " + targetPoint.y);
         try {
             if (map != null) {
                 //Log.i("=============", "Tested cell:" + x + " " + y);
                 return map[x][y].isPassable();
             }
-        }catch (IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             return false;
         }
         return false;
     }
 
-    public Point getCenter(){
-        if(map != null) {
-            return new Point(TILE_SIZE.getWidth() * map.length / 2, TILE_SIZE.getHeight() * map[0].length / 2);
+    /* Returns the center of the map. */
+    public Point getCenter() {
+        if (map != null) {
+            return new Point(SIZE * map.length / 2, SIZE * map[0].length / 2);
         }
-        return new Point(0,0);
+        return new Point(0, 0);
     }
 
-
-    //creates a map based on a given file, which has
-    //numbers that corresponds to a specific image and rotation
-    private void loadMap(String filename){
+    /* Creates a map based on a given file, which has numbers that corresponds to a specific image
+     * and rotation,
+     */
+    private void loadMap(String filename) {
         int height, width;
-        Point point = new Point(0,0);
+        Point point = new Point(0, 0);
 
         Scanner scanner;
         try {
-//            scanner = new Scanner(new File(filename));
+            /* Opens the csv file from the assets folder. */
             scanner = new Scanner(context.getAssets().open(filename)).useDelimiter(",|\r?\n");
 
         } catch (FileNotFoundException e) {
             Log.i("============", "Unable to load " + filename);
             Toast.makeText(context, "Unable to load map", Toast.LENGTH_SHORT).show();
             scanner = null;
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             scanner = null;
         }
 
-        if(scanner != null) {
+        /* Reads the csv file line by line.*/
+        if (scanner != null) {
             height = scanner.nextInt();
             width = scanner.nextInt();
 
@@ -104,10 +107,13 @@ public class Map {
             scanner.close();
         }
     }
-    private WorldObject loadMapHelper(int objectNumber, Point point){
+
+    /* Assigns each map tile to the proper drawable and rotation */
+    private WorldObject loadMapHelper(int objectNumber, Point point) {
         int drawableId = 0;
         int rotation = 0;
         boolean passable = false;
+        WorldObject worldObject;
 
         /*
             1 = floor
@@ -115,24 +121,27 @@ public class Map {
             6-9 = door
             10-13 = wall_corner
          */
-        if(objectNumber == 1) {
+        if (objectNumber == 1) {
             drawableId = R.drawable.floor;
             passable = true;
-        }else if(objectNumber >= 2 && objectNumber <= 5){
+        } else if (objectNumber >= 2 && objectNumber <= 5) {
             drawableId = R.drawable.wall;
             rotation = ((objectNumber + 2) % 4) * 90;
-        }else if(objectNumber >=10 && objectNumber <= 13){
+        } else if (objectNumber >= 10 && objectNumber <= 13) {
             drawableId = R.drawable.wall_corner;
             rotation = ((objectNumber - 6) % 4) * 90;
         }
 
-        WorldObject worldObject = new WorldObject(point,rotation,TILE_SIZE, context, passable);
-        if(drawableId != 0) {
-            worldObject.setDrawable(drawableId);
+        worldObject = new WorldObject(point, rotation, new Size(SIZE,SIZE), context, passable);
+        if (drawableId != 0) {
+            worldObject.setDrawableByID(drawableId);
         }
-        return  worldObject;
+        return worldObject;
     }
 
+
+    /*
+     * Legacy code from testing prior to loadMap implementation.
     //Creates a one room map for testing purposes.
     public void makeTestMap() {
         map = new WorldObject[4][4];
@@ -144,46 +153,46 @@ public class Map {
         map[3][0] = new WorldObject(new Point(3 * TILE_SIZE.getWidth(), 0), 270, TILE_SIZE, context, false);
 
 
-        map[0][0].setDrawable(R.drawable.wall_corner);
-        map[0][3].setDrawable(R.drawable.wall_corner);
-        map[3][3].setDrawable(R.drawable.wall_corner);
-        map[3][0].setDrawable(R.drawable.wall_corner);
+        map[0][0].setDrawableByID(R.drawable.wall_corner);
+        map[0][3].setDrawableByID(R.drawable.wall_corner);
+        map[3][3].setDrawableByID(R.drawable.wall_corner);
+        map[3][0].setDrawableByID(R.drawable.wall_corner);
 
         //Top Wall
         map[1][0] = new WorldObject(new Point(TILE_SIZE.getWidth(), 0), 0, TILE_SIZE, context, false);
         map[2][0] = new WorldObject(new Point(2 * TILE_SIZE.getWidth(), 0), 0, TILE_SIZE, context, false);
-        map[1][0].setDrawable(R.drawable.wall);
-        map[2][0].setDrawable(R.drawable.wall);
+        map[1][0].setDrawableByID(R.drawable.wall);
+        map[2][0].setDrawableByID(R.drawable.wall);
 
         //Left Wall
         map[0][1] = new WorldObject(new Point(0, TILE_SIZE.getHeight()), 90, TILE_SIZE, context, false);
         map[0][2] = new WorldObject(new Point(0, 2 * TILE_SIZE.getHeight()), 90, TILE_SIZE, context, false);
-        map[0][1].setDrawable(R.drawable.wall);
-        map[0][2].setDrawable(R.drawable.wall);
+        map[0][1].setDrawableByID(R.drawable.wall);
+        map[0][2].setDrawableByID(R.drawable.wall);
 
         //Bottom Wall
         map[1][3] = new WorldObject(new Point(TILE_SIZE.getWidth(), 3 * TILE_SIZE.getHeight()), 180, TILE_SIZE, context, false);
         map[2][3] = new WorldObject(new Point(2 * TILE_SIZE.getWidth(), 3 * TILE_SIZE.getHeight()), 180, TILE_SIZE, context, false);
-        map[1][3].setDrawable(R.drawable.wall);
-        map[2][3].setDrawable(R.drawable.wall);
+        map[1][3].setDrawableByID(R.drawable.wall);
+        map[2][3].setDrawableByID(R.drawable.wall);
 
         //Right Wall
         map[3][1] = new WorldObject(new Point(3 * TILE_SIZE.getWidth(), TILE_SIZE.getHeight()), 270, TILE_SIZE, context, false);
         map[3][2] = new WorldObject(new Point(3 * TILE_SIZE.getWidth(), 2 * TILE_SIZE.getHeight()), 270, TILE_SIZE, context, false);
-        map[3][1].setDrawable(R.drawable.wall);
-        map[3][2].setDrawable(R.drawable.wall);
+        map[3][1].setDrawableByID(R.drawable.wall);
+        map[3][2].setDrawableByID(R.drawable.wall);
 
         //Center
         map[1][1] = new WorldObject(new Point(TILE_SIZE.getWidth(), TILE_SIZE.getHeight()), 0, TILE_SIZE, context, true);
         map[1][2] = new WorldObject(new Point(TILE_SIZE.getWidth(), 2 * TILE_SIZE.getHeight()), 0, TILE_SIZE, context, true);
-        map[2][1] = new WorldObject(new Point(2 * TILE_SIZE.getWidth(),TILE_SIZE.getHeight()), 0, TILE_SIZE, context, true);
+        map[2][1] = new WorldObject(new Point(2 * TILE_SIZE.getWidth(), TILE_SIZE.getHeight()), 0, TILE_SIZE, context, true);
         map[2][2] = new WorldObject(new Point(2 * TILE_SIZE.getWidth(), 2 * TILE_SIZE.getHeight()), 0, TILE_SIZE, context, true);
 
-        map[1][1].setDrawable(R.drawable.floor);
-        map[1][2].setDrawable(R.drawable.floor);
-        map[2][1].setDrawable(R.drawable.floor);
-        map[2][2].setDrawable(R.drawable.floor);
+        map[1][1].setDrawableByID(R.drawable.floor);
+        map[1][2].setDrawableByID(R.drawable.floor);
+        map[2][1].setDrawableByID(R.drawable.floor);
+        map[2][2].setDrawableByID(R.drawable.floor);
     }
-
+    */
 
 }
